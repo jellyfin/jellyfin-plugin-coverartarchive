24e5b8a4edd75d59e56e820edb5b709c9969556d
grafixeyehero <32230989+grafixeyehero@users.noreply.github.com>
2021-05-15 01:51:31 +0300

    fix plugin settings to  save correctly

--- Jellyfin.Plugin.CoverArtArchive/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.CoverArtArchive/Configuration/PluginConfiguration.cs
@@ -3,9 +3,28 @@ using MediaBrowser.Model.Plugins;
 namespace Jellyfin.Plugin.CoverArtArchive.Configuration
 {
     /// <summary>
-    /// The (empty) plugin configuration.
+    /// Class PluginConfiguration.
     /// </summary>
     public class PluginConfiguration : BasePluginConfiguration
     {
+        /// <summary>
+        /// Gets or sets options.
+        /// </summary>
+        public string Options { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets an integer.
+        /// </summary>
+        public int AnInteger { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether it true or false setting.
+        /// </summary>
+        public bool TrueFalseSetting { get; set; }
+
+        /// <summary>
+        /// Gets or sets a string.
+        /// </summary>
+        public string AString { get; set; } = string.Empty;
     }
 }
