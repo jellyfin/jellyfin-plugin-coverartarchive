4b427f130efbcfdffb3bb9d506443db1a21c2fa0
Julien Machiels <julien.machiels@protonmail.com>
2021-08-01 18:38:29 +0200

    Update Jellyfin.Plugin.CoverArtArchive/CoverArtArchiveImageProvider.cs
    Co-authored-by: Cody Robibero <cody@robibe.ro>

--- Jellyfin.Plugin.CoverArtArchive/CoverArtArchiveImageProvider.cs
+++ Jellyfin.Plugin.CoverArtArchive/CoverArtArchiveImageProvider.cs
@@ -81,7 +81,7 @@ namespace Jellyfin.Plugin.CoverArtArchive
                 {
                     var release = await _coverArtClient.FetchReleaseAsync(Guid.Parse(musicBrainzId)).ConfigureAwait(false);
 
-                    var frontImage = release.Images.SingleOrDefault(image => image.Types == CoverArtType.Front);
+                    var frontImage = release.Images.FirstOrDefault(image => image.Types == CoverArtType.Front);
 
                     if (frontImage is not null)
                     {
